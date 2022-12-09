6ee7baa7523c62c7fc8ca4425585b993148a71b4
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-09 18:14:03 +0300

    update files

--- bin/Programmer_Day.js
+++ bin/Programmer_Day.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import StartQuiz from '../src/Quiz.js';
+import StartQuiz from '../src/HelloQuiz.js';
 
 console.log('Welcome to the "Programmer Day" Quiz.');
 console.log(StartQuiz());
